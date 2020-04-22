32fbe2ec946395c753d95b62720bd30a0be11258
deivy-spaulonci <deivy.spaulonci@hotmail.com>
2020-04-22 01:47:49 -0300

    teste de repositorio

--- src/main/java/com/br/vxassist/VxPayofApplication.java
+++ src/main/java/com/br/vxassist/VxPayofApplication.java
@@ -1,7 +1,5 @@
 package com.br.vxassist;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,7 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories("com.br.vxassist.repository")
 public class VxPayofApplication {
 
-    private static final Logger logger = LoggerFactory.getLogger(VxPayofApplication.class);
+    //private static final Logger logger = LoggerFactory.getLogger(VxPayofApplication.class);
 
 //    @PersistenceContext
 //    private EntityManager entityManager;
@@ -24,6 +22,8 @@ public class VxPayofApplication {
     @Bean
     public CommandLineRunner demo() {
         return (args) -> {
+
+            System.out.println("iniciando o systema...");
             //throw new GenericErrorException("Código de Barras inválido");
             //                QTipoDespesa qTipoDespesa = QTipoDespesa.tipoDespesa;
 //                JPAQuery<?> query = new JPAQuery<Void>(entityManager);
