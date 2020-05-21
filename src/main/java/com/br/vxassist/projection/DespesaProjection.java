aba909b073bd3b81a8e2502b1f06f85524c9c4aa
deivy-spaulonci <deivy.spaulonci@hotmail.com>
2020-05-20 23:15:01 -0300

    teste sort url in projections

--- src/main/java/com/br/vxassist/projection/DespesaProjection.java
+++ src/main/java/com/br/vxassist/projection/DespesaProjection.java
@@ -1,6 +1,7 @@
 package com.br.vxassist.projection;
 
 import com.br.vxassist.model.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.rest.core.config.Projection;
 
 import java.math.BigDecimal;
@@ -10,12 +11,14 @@ import java.util.List;
 @Projection(name = "depesaProjection", types = { Despesa.class })
 public interface DespesaProjection {
 
+    @Value("#{target.id}")
     Long getId();
     TipoDespesa getTipoDespesa();
-    Fornecedor getFornecedor();
-    Date getData();
-    FormaPagamento getFormaPagamento();
-    BigDecimal getValor();
-    String getObs();
-    List<InformacaoExtra> getInformacaoExtra();
+//    Fornecedor getFornecedor();
+//    Date getData();
+//    FormaPagamento getFormaPagamento();
+//    BigDecimal getValor();
+//    String getObs();
+//    List<InformacaoExtra> getInformacaoExtra();
+
 }
