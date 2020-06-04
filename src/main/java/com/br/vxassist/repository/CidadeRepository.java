3102d41b22ef0a8d45bbbfb535c84864b3896401
deivy-spaulonci <deivy.spaulonci@hotmail.com>
2020-06-04 02:35:58 -0300

    ajuste ordenacao

--- src/main/java/com/br/vxassist/repository/CidadeRepository.java
+++ src/main/java/com/br/vxassist/repository/CidadeRepository.java
@@ -2,9 +2,14 @@ package com.br.vxassist.repository;
 
 import com.br.vxassist.model.Cidade;
 import com.br.vxassist.projection.CidadeProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(excerptProjection = CidadeProjection.class, path = "cidade")
 public interface CidadeRepository extends PagingAndSortingRepository<Cidade, Long> {
+
 }
--- src/main/java/com/br/vxassist/repository/DespesaRepository.java
+++ src/main/java/com/br/vxassist/repository/DespesaRepository.java
@@ -2,15 +2,22 @@ package com.br.vxassist.repository;
 
 import com.br.vxassist.model.Despesa;
 import com.br.vxassist.projection.DespesaProjection;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 @RepositoryRestResource(excerptProjection = DespesaProjection.class, path = "despesa")
-public interface DespesaRepository extends JpaRepository<Despesa, Long> {
-//    @RestResource(path = "sortByTipoDespesaNome")
-//    @Query("SELECT d FROM Despesa d ORDER BY d.tipoDespesa.nome ASC")
-//      List<Despesa> findItemsLessThanAndType(Page page);
+public interface DespesaRepository extends PagingAndSortingRepository<Despesa, Long> {
 
+    @RestResource(rel = "findByOrderByTipoDespesaNome", path = "findByOrderByTipoDespesaNome")
+    List<Despesa> findByOrderByTipoDespesaNome(Pageable page);
+
+
+    @RestResource(rel = "findByOrderByFornecedorNome", path = "findByOrderByFornecedorNome")
+    List<Despesa> findByOrderByFornecedorNome(Pageable page);
 
 }
