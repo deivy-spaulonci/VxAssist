18e80c13b80dbeab5dd574b41f09954406aa10fd
deivy-spaulonci <deivy.spaulonci@hotmail.com>
2020-06-04 22:54:39 -0300

    ajuste sort

--- src/main/java/com/br/vxassist/repository/DespesaRepository.java
+++ src/main/java/com/br/vxassist/repository/DespesaRepository.java
@@ -4,6 +4,7 @@ import com.br.vxassist.model.Despesa;
 import com.br.vxassist.projection.DespesaProjection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
@@ -14,7 +15,7 @@ import java.util.List;
 public interface DespesaRepository extends PagingAndSortingRepository<Despesa, Long> {
 
     @RestResource(rel = "findByOrderByTipoDespesaNome", path = "findByOrderByTipoDespesaNome")
-    List<Despesa> findByOrderByTipoDespesaNome(Pageable page);
+    Page<Despesa> findByOrderByTipoDespesaNome(Pageable page);
 
 
     @RestResource(rel = "findByOrderByFornecedorNome", path = "findByOrderByFornecedorNome")
